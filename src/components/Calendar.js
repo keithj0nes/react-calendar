78b962e5c9e120fa9d5181f8c6a225fd21c208fd
keith <keith@raincityambience.com>
2018-06-06 22:30:57 -0700

    add / subtract months via click working

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import dateFns from 'date-fns';
+
 class Calendar extends Component {
 
   state = {
@@ -9,7 +11,25 @@ class Calendar extends Component {
 
 
   renderHeader(){
+    const dateFormat = "MMMM YYYY";
 
+    return (
+      <div className="header row flex-middle">
+        <div className="col col-start">
+          <div className="icon" onClick={this.prevMonth}>
+            chevron_left
+          </div>
+        </div>
+        <div className="col col-center">
+          <span>
+            {dateFns.format(this.state.currentMonth, dateFormat)}
+          </span>
+        </div>
+        <div className="col col-end" onClick={this.nextMonth}>
+          <div className="icon">chevron_right</div>
+        </div>
+      </div>
+    );
   }
 
   renderDays(){
@@ -19,17 +39,37 @@ class Calendar extends Component {
   renderCells(){
 
   }
-  
+
+
+  onDateClick = day => {
+
+  }
+
+  nextMonth = () => {
+    // console.log(this.state.currentMonth);
+    // console.log(dateFns.addMonths(this.state.currentMonth, 1));
+    this.setState({
+      currentMonth: dateFns.addMonths(this.state.currentMonth, 1)
+    });
+  };
+
+  prevMonth = () => {
+    this.setState({
+      currentMonth: dateFns.subMonths(this.state.currentMonth, 1)
+    });
+  };
+
+
   render(){
 
 
 
 
     return (
-      <div>
-        <div>Header</div>
-        <div>Days</div>
-        <div>Cells</div>
+      <div className="calendar">
+        {this.renderHeader()}
+        {this.renderDays()}
+        {this.renderCells()}
       </div>
     )
   }
